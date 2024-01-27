b1c9f6d566c7314cc328bce90d3c50c5c537b0c2
Tim Tanida <tanida.tim@googlemail.com>
2024-01-27 23:33:30 +0100

    Save progress

--- src/useWhisper.ts
+++ src/useWhisper.ts
@@ -387,55 +387,56 @@ export const useWhisper: UseWhisperHook = (config) => {
         let blob = await recorder.current.getBlob()
         let transcribed_message = ''
 
-        return "Hi, how are you?";
+        if (removeSilence) {
+          const { createFFmpeg } = await import('@ffmpeg/ffmpeg')
+          const ffmpeg = createFFmpeg({
+            mainName: 'main',
+            corePath: ffmpegCoreUrl,
+            log: true,
+          })
+          if (!ffmpeg.isLoaded()) {
+            await ffmpeg.load()
+          }
+          const buffer = await blob.arrayBuffer()
+          console.log({ in: buffer.byteLength })
+          ffmpeg.FS('writeFile', 'in.wav', new Uint8Array(buffer))
+          await ffmpeg.run(
+            '-i', // Input
+            'in.wav',
+            '-acodec', // Audio codec
+            'libmp3lame',
+            '-b:a', // Audio bitrate
+            '96k',
+            '-ar', // Audio sample rate
+            '44100',
+            '-af', // Audio filter = remove silence from start to end with 2 seconds in between
+            silenceRemoveCommand,
+            'out.mp3' // Output
+          )
 
-        // if (removeSilence) {
-        //   const { createFFmpeg } = await import('@ffmpeg/ffmpeg')
-        //   const ffmpeg = createFFmpeg({
-        //     mainName: 'main',
-        //     corePath: ffmpegCoreUrl,
-        //     log: true,
-        //   })
-        //   if (!ffmpeg.isLoaded()) {
-        //     await ffmpeg.load()
-        //   }
-        //   const buffer = await blob.arrayBuffer()
-        //   console.log({ in: buffer.byteLength })
-        //   ffmpeg.FS('writeFile', 'in.wav', new Uint8Array(buffer))
-        //   await ffmpeg.run(
-        //     '-i', // Input
-        //     'in.wav',
-        //     '-acodec', // Audio codec
-        //     'libmp3lame',
-        //     '-b:a', // Audio bitrate
-        //     '96k',
-        //     '-ar', // Audio sample rate
-        //     '44100',
-        //     '-af', // Audio filter = remove silence from start to end with 2 seconds in between
-        //     silenceRemoveCommand,
-        //     'out.mp3' // Output
-        //   )
-        //   const out = ffmpeg.FS('readFile', 'out.mp3')
-        //   console.log({ out: out.buffer.byteLength })
+          return "Hi, where are you?";
 
-        //   // 225 seems to be empty mp3 file
-        //   if (out.length <= 225) {
-        //     ffmpeg.exit()
-        //     setTranscript({
-        //       blob,
-        //     })
-        //     setTranscribing(false)
-        //     return "There was an error 1"
-        //   }
-        //   blob = new Blob([out.buffer], { type: 'audio/mpeg' })
-        //   ffmpeg.exit()
-        // } else {
-        //   const buffer = await blob.arrayBuffer()
-        //   console.log({ wav: buffer.byteLength })
-        //   const mp3 = encoder.current.encodeBuffer(new Int16Array(buffer))
-        //   blob = new Blob([mp3], { type: 'audio/mpeg' })
-        //   console.log({ blob, mp3: mp3.byteLength })
-        // }
+          // const out = ffmpeg.FS('readFile', 'out.mp3')
+          // console.log({ out: out.buffer.byteLength })
+
+          // // 225 seems to be empty mp3 file
+          // if (out.length <= 225) {
+          //   ffmpeg.exit()
+          //   setTranscript({
+          //     blob,
+          //   })
+          //   setTranscribing(false)
+          //   return "There was an error 1"
+          // }
+          // blob = new Blob([out.buffer], { type: 'audio/mpeg' })
+          // ffmpeg.exit()
+        } else {
+          const buffer = await blob.arrayBuffer()
+          console.log({ wav: buffer.byteLength })
+          const mp3 = encoder.current.encodeBuffer(new Int16Array(buffer))
+          blob = new Blob([mp3], { type: 'audio/mpeg' })
+          console.log({ blob, mp3: mp3.byteLength })
+        }
 
         // if (typeof onTranscribeCallback === 'function') {
         //   const transcribed = await onTranscribeCallback(blob)
